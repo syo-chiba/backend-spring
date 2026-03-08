c80a497b030edf60ce082366caefe45f4f54d7c1
shochiba2 <blacklagoon630803@gmail.com>
2026-03-08 12:38:49 +0900

    時間重複の修正対応

--- src/main/java/com/example/backend_spring/repository/StepCandidateRepository.java
+++ src/main/java/com/example/backend_spring/repository/StepCandidateRepository.java
@@ -36,7 +36,8 @@ public interface StepCandidateRepository extends JpaRepository<StepCandidate, Lo
             INNER JOIN flows f ON f.id = fs.flow_id
             LEFT JOIN participants p ON p.id = fs.participant_id
             WHERE ((:createdByUserId IS NULL AND f.created_by_user_id IS NULL) OR f.created_by_user_id = :createdByUserId)
-              AND sc.status IN ('PROPOSED', 'SELECTED')
+              AND fs.status = 'ACTIVE'
+              AND sc.status = 'PROPOSED'
               AND (:excludeFlowStepId IS NULL OR sc.flow_step_id <> :excludeFlowStepId)
               AND sc.start_at < :newEndAt
               AND sc.end_at > :newStartAt
